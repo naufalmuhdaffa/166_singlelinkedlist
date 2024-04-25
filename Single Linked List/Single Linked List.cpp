54232a4bdb2a3cbe7d098ed0942d327b65c0f28e
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-04-25 11:06:08 +0700

    menambahkan struktur boolean

--- Single Linked List/Single Linked List.cpp
+++ Single Linked List/Single Linked List.cpp
@@ -69,6 +69,10 @@ bool serachNode(int nim, Node* current, Node* previous) {
         return false;
     }
 }
+bool deleteNode(int nim) {
+    Node* current = START;
+    Node* previous = START;
+}
 
 int main()
 {
